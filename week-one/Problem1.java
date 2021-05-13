5cacd9cc312e5c87bcf8b827896be5e981e535aa
Diego Tavera <dtavera@gmail.com>
2021-05-12 23:50:30 -0500

    problem 1 solution

--- week-one/Problem1.java
+++ week-one/Problem1.java
@@ -1,6 +1,27 @@
-
 class Problem1{
+
+    static boolean isPalindromeSentence(String input){
+        int inputLength = input.length();
+        StringBuilder inputWithoutAcute= new StringBuilder(); 
+        for (int i = 0; i < inputLength; i++) {
+            char letter = input.charAt(i);
+            if ((letter >= 'A' && letter <= 'Z') || (letter >= 'a' && letter <= 'z')) {
+                inputWithoutAcute.append(Character.toLowerCase(letter));
+            }
+        }
+
+        int inputSize = inputWithoutAcute.length();
+        for(int i = 0; i < inputSize / 2; i++) {
+            if(inputWithoutAcute.charAt(i) != inputWithoutAcute.charAt(inputSize -i -1)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
     public static void main(String[] args){
-        System.out.println("Hi Devz");
+        String input = "La ruta nos aportó otro paso natural";
+        System.out.println(isPalindromeSentence(input));
     }
 }
